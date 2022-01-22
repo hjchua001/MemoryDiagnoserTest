16e5315de92cb6d6daea6532d7e39749aa7f76f5
hjchua001 <33489234+hjchua001@users.noreply.github.com>
2022-01-23 02:41:07 +0800

    Update ExceptionStringProgram.cs

--- ExceptionToString/ExceptionStringProgram.cs
+++ ExceptionToString/ExceptionStringProgram.cs
@@ -39,7 +39,7 @@ public class ExceptionStringProgram
         }
         catch (Exception ex)
         {
-            return JsonConvert.SerializeObject(ex)";
+            return JsonConvert.SerializeObject(ex);
         }
     }
 }
